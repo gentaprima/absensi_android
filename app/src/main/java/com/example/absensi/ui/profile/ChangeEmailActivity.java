8439ad2a654ccc8a827d43ef20e951dff5f95e29
gentaprima <gentaprima600@gmail.com>
2020-12-02 19:53:10 +0700

    update banyak

---
+++ app/src/main/java/com/example/absensi/ui/profile/ChangeEmailActivity.java
@@ -0,0 +1,16 @@
+package com.example.absensi.ui.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.absensi.R;
+
+public class ChangeEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_email);
+    }
+}
